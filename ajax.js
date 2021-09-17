993628dd16fc54bdc67ca32196a77d3d00ed0cec
Cristian Sancricca <cristiansancricca@gmail.com>
2021-09-16 23:50:41 -0300

    API Fetch + Async-Await

--- ajax.js
+++ ajax.js
@@ -58,3 +58,31 @@
       )
     );
 })();
+
+(() => {
+  const $fetchAsync = document.getElementById("fetch-async");
+  const $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await fetch("https://jsonplaceholder.typicode.com/users");
+      let json = await res.json();
+
+      if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `Nombre: ${el.name}--Mail: ${el.email}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetchAsync.appendChild($fragment);
+    } catch (err) {
+      let message = err.statusText || "Ocurrio un error";
+      $fetchAsync.innerHTML = `Error ${err.status}:${message}`;
+    } finally {
+    }
+  }
+
+  getData();
+})();
