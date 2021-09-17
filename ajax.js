9e9e6cbdb8a066bff63de4a3842c2de7be4b6713
Cristian Sancricca <cristiansancricca@gmail.com>
2021-09-16 22:57:59 -0300

    API FETCH

--- ajax.js
+++ ajax.js
@@ -29,3 +29,32 @@
 
   xhr.send();
 })();
+
+/*API FETCH*/
+
+(() => {
+  const $fetch = document.getElementById("fetch");
+  const $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `Nombre: ${el.name}--Mail: ${el.email}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      console.log(err);
+      let message = err.statusText || "Ocurrio un error";
+      $fetch.innerHTML = `Error ${err.status}:${message}`;
+    })
+    .finally(() =>
+      console.log(
+        "El finally se ejecuta independiente del resultado de la promesa fetch"
+      )
+    );
+})();
